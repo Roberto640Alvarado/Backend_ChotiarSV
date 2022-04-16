28c67c48d0f8401adfcf48fdfed0c46aaeee208b
Roberto640Alvarado <00176620@uca.edu.sv>
2022-04-16 10:12:44 -0600

    Add: Request Post

--- Chotiar-2022/src/controllers/post.controller.ts
+++ Chotiar-2022/src/controllers/post.controller.ts
@@ -19,6 +19,73 @@ const postController = {
         } catch (error: any) {
             return res.status(error.status ?? 400).json({ message: error.message ?? JSON.stringify(error) });
         }
+    },
+    //Obtenemos todos los posts
+    getAll: async (req: Request, res: Response) => {
+
+        //Paginacion
+        const { page, limit } = req.query;
+        const pageNumber = +(page as string);
+        const limitNumber = +(limit as string);
+
+        const post = await Post
+            .find()
+            .limit(+limitNumber)
+            .skip((pageNumber - 1) * limitNumber);
+
+        const postCount = await Post.countDocuments();//Cantidad de post
+
+        const pagingInfo = {
+            total: postCount,
+            page: pageNumber,
+            perpage: limitNumber,
+            pages: Math.ceil(postCount / limitNumber)
+        }
+
+        return res.status(201).json({ message: 'Success', content: post, pagingInfo });
+
+    },
+    //Obtener solo un post
+    getOne: async (req: Request, res: Response) => {
+        try {
+            return res.status(200).json({
+                message: "Done",
+                content: await Post.findOne({ _id: req.params._id })
+            })
+
+        } catch (err: any) {
+            return res
+                .status(err.status as number ?? 400)
+                .json({ message: err.message ?? JSON.stringify(err) });
+        }
+    },
+    //Eliminar un post
+    delete: async (req: Request, res: Response) => {
+        try {
+            return res.status(200).json({
+                message: "Done",
+                content: await Post.findOneAndDelete({ _id: req.params._id })
+            })
+
+        } catch (err: any) {
+            return res
+                .status(err.status as number ?? 400)
+                .json({ message: err.message ?? JSON.stringify(err) });
+        }
+    },
+    //Buscar post por Titulo
+    search: async (req: Request, res: Response) => {
+        try {
+
+            const { title } = req.params;
+            Post.find({ title: { $regex: title, $options: "i" } })
+                .then((posts) => res.status(200).json({ posts }))
+
+        } catch (error: any) {
+            return res
+                .status(error.status as number ?? 400)
+                .json({ message: error.message ?? JSON.stringify(error) });
+        }
     }
 }
 export default postController;
--- Chotiar-2022/src/routes/post.routes.ts
+++ Chotiar-2022/src/routes/post.routes.ts
@@ -6,6 +6,10 @@ const baseRouter = Router();
 
 //Peticiones a base del Router
 baseRouter.post('/create', postController.create);
+baseRouter.get('/getAll', postController.getAll);
+baseRouter.get('/getOne/:_id', postController.getOne);
+baseRouter.delete('/delete/:_id', postController.delete);
+baseRouter.get('/search/:title', postController.search);
 
 
 const postRouter = Router();
